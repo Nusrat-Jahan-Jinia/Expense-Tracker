7377fc2edf03447e48b832ad0a0c196860c3bb91
nusrat <nusratcode14@gmail.com>
2024-01-31 14:14:08 +0600

    misc added

--- src/main/java/com/example/expensetracker/controller/CustomerOrderController.java
+++ src/main/java/com/example/expensetracker/controller/CustomerOrderController.java
@@ -4,6 +4,7 @@ import com.example.expensetracker.entity.*;
 import com.example.expensetracker.repository.CustomerOrderRepository;
 import com.example.expensetracker.repository.CustomerRepository;
 import com.example.expensetracker.repository.ProductRepository;
+import com.example.expensetracker.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,19 +20,26 @@ public class CustomerOrderController {
     private final CustomerRepository customerRepository;
     private final ProductRepository productRepository;
     private final CustomerOrderRepository customerOrderRepository;
+    private final OrderService orderService;
 
     @Autowired
-    public CustomerOrderController(CustomerRepository customerRepository, ProductRepository productRepository, CustomerOrderRepository customerOrderRepository) {
+    public CustomerOrderController(
+            CustomerRepository customerRepository,
+            ProductRepository productRepository,
+            CustomerOrderRepository customerOrderRepository,
+            OrderService orderService
+    ) {
         this.customerRepository = customerRepository;
         this.productRepository = productRepository;
         this.customerOrderRepository = customerOrderRepository;
+        this.orderService = orderService;
     }
 
     @GetMapping("")
     public ModelAndView home(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("data", customerOrderRepository.findAll());
-        System.out.println(customerOrderRepository.findAll());
+//        System.out.println(customerOrderRepository.findAll());
         modelAndView.setViewName("order/list.html");
         return modelAndView;
     }
@@ -53,6 +61,40 @@ public class CustomerOrderController {
     public String submitCreate(CustomerOrder customerOrder, Model model){
         customerOrderRepository.save(customerOrder);
         return "redirect:/orders";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteEntity(@PathVariable int id) {
+        customerOrderRepository.deleteById(id);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/{id}/update")
+    public ModelAndView update(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView();
+        List<Customer> customers = customerRepository.findAll();
+        List<Product> products = productRepository.findAll();
+        modelAndView.addObject("id", id);
+        modelAndView.addObject("dto", orderService.getOrderById(id));
+
+        modelAndView.addObject("customers", customers);
+        modelAndView.addObject("products", products);
+        modelAndView.addObject("method", "put");
+        modelAndView.setViewName("order/edit.html");
+        return modelAndView;
+    }
+
+    @PutMapping(value = "/{id}")
+    public ModelAndView submitUpdate(@PathVariable("id") int id, CustomerOrder customerOrder, Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        boolean success = orderService.edit(customerOrder, id);
+        if (!success) {
+            model.addAttribute("result", "Something went wrong!");
+        } else {
+            model.addAttribute("result", "Successfully data updated!");
+        }
+        modelAndView.setViewName("order/list.html");
+        return modelAndView;
     }
 
 }
--- src/main/java/com/example/expensetracker/service/OrderService.java
+++ src/main/java/com/example/expensetracker/service/OrderService.java
@@ -1,9 +1,30 @@
 package com.example.expensetracker.service;
 
+import com.example.expensetracker.entity.CustomerOrder;
+import com.example.expensetracker.entity.Income;
+import com.example.expensetracker.repository.CustomerOrderRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
 
+    private final CustomerOrderRepository customerOrderRepository;
+    public OrderService(CustomerOrderRepository customerOrderRepository){
+        this.customerOrderRepository = customerOrderRepository;
+    }
+
+    public CustomerOrder getOrderById(int id) {
+        return customerOrderRepository.findById(id).orElse(null);
+    }
+
+    public boolean edit(CustomerOrder customerOrder, int id) {
+        try {
+            customerOrder.setId(id);
+            customerOrderRepository.save(customerOrder);
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
 
 }
