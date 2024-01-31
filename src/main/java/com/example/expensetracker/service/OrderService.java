58baac917e5cb4e4afcb6bccfd27986b9c5ecf58
nusrat <nusratcode14@gmail.com>
2024-01-31 15:41:27 +0600

    Add exception handling in edit method of OrderService

--- src/main/java/com/example/expensetracker/service/OrderService.java
+++ src/main/java/com/example/expensetracker/service/OrderService.java
@@ -1,9 +1,12 @@
 package com.example.expensetracker.service;
 
 import com.example.expensetracker.entity.CustomerOrder;
+import com.example.expensetracker.exception.CustomerOrderUpdateException;
 import com.example.expensetracker.entity.Income;
 import com.example.expensetracker.repository.CustomerOrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class OrderService {
@@ -17,14 +20,19 @@ public class OrderService {
         return customerOrderRepository.findById(id).orElse(null);
     }
 
-    public boolean edit(CustomerOrder customerOrder, int id) {
-        try {
-            customerOrder.setId(id);
-            customerOrderRepository.save(customerOrder);
-            return true;
-        } catch (Exception exception) {
-            return false;
+    public void edit(CustomerOrder updatedCustomerOrder, int id) {
+        Optional<CustomerOrder> existingOrderOptional = customerOrderRepository.findById(id);
+
+        if (existingOrderOptional.isPresent()) {
+            CustomerOrder existingOrder = existingOrderOptional.get();
+            existingOrder.setOrderBy(updatedCustomerOrder.getOrderBy());
+            existingOrder.setOrderDate(updatedCustomerOrder.getOrderDate());
+            existingOrder.setCustomer(updatedCustomerOrder.getCustomer());
+            existingOrder.setProducts(updatedCustomerOrder.getProducts());
+
+            customerOrderRepository.save(existingOrder);
+        } else {
+            throw new CustomerOrderUpdateException("Customer order with id " + id + " not found");
         }
     }
-
 }
