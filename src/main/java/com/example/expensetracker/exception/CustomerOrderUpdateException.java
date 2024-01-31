b2dd861b608af39bac59bfcaf1005a9f721a9ca2
nusrat <nusratcode14@gmail.com>
2024-01-31 15:37:00 +0600

    Add custom exception class to indicate failed order update

---
+++ src/main/java/com/example/expensetracker/exception/CustomerOrderUpdateException.java
@@ -0,0 +1,8 @@
+package com.example.expensetracker.exception;
+
+public class CustomerOrderUpdateException extends RuntimeException{
+
+    public CustomerOrderUpdateException(String message) {
+        super(message);
+    }
+}
