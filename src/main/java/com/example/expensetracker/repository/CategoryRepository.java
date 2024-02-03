3892baa43ee152cbaf707a4e2b504fed64dea442
nusrat <nusratcode14@gmail.com>
2024-02-04 03:09:31 +0600

    add jpa data rest at category repository

--- src/main/java/com/example/expensetracker/repository/CategoryRepository.java
+++ src/main/java/com/example/expensetracker/repository/CategoryRepository.java
@@ -2,8 +2,10 @@ package com.example.expensetracker.repository;
 
 import com.example.expensetracker.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
+@RepositoryRestResource(collectionResourceRel = "categoryList", path = "categoryList")
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
