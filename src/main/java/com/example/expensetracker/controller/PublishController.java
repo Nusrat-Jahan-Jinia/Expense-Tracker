a1f97b73da5b8fb343d8affea5842b92828c11bc
nusrat <nusratcode14@gmail.com>
2024-02-07 04:27:19 +0600

    misc

--- src/main/java/com/example/expensetracker/controller/PublishController.java
+++ src/main/java/com/example/expensetracker/controller/PublishController.java
@@ -1,7 +1,6 @@
 package com.example.expensetracker.controller;
 
 import com.example.expensetracker.entity.SystemMessage;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jms.core.JmsTemplate;
